28e8d046d5cb4bb28b9c1facb20b13f27016ad8d
Mantu Pani <mantu.pani@corp.litmusworld.com>
2020-06-14 02:16:26 +0530

    all task done

--- controller/AuthorController.js
+++ controller/AuthorController.js
@@ -28,7 +28,6 @@ const AuthorController = {
     getAuthorsByAwardYear: function(db, where, cb){
         const Authors = db.collection('authors');
         where = where || {};
-        console.log(where)
         Authors.find(where).project({author_name:1, author_address:1, mobile:1, email:1}).toArray(function(err, docs) {
             if(err){
                 cb(err, []);
@@ -37,6 +36,26 @@ const AuthorController = {
             console.log("Found the following records");
             cb(null, docs);
           });
+    },
+
+    getAllTotalProfitByAuthor : function(db, where, afterWhere, cb){
+        const Authors = db.collection('authors');
+        where = where || {};
+        afterWhere = afterWhere || {};
+        Authors.aggregate([
+            {$match:where},
+            {$unwind:"$sold_books"},
+            {$group:{_id:"$_id" , 
+                "totalBooksSold":{$sum:"$sold_books.price"} , 
+                "totalProfit":{$sum: { $multiply: [ "$sold_books.price", "$sold_books.totalSold" ] } }
+            }} ,
+            {$match:afterWhere} 
+        ]).toArray(function(err, results) {
+            if(err){
+                cb(err, []);
+            }
+            cb(null, results);
+        });
     }
 
 };
--- routes/api.js
+++ routes/api.js
@@ -2,53 +2,249 @@ const express = require('express');
 const router = express.Router();
 const client = require('./../controller/DBController');
 const AuthorController = require('./../controller/AuthorController');
+const { ObjectId } = require('mongodb');
 
 router.get('/author', function(req, res){
     const db = client.db('my-test-db');
     const authors = [
         {
-            author_name:"mantu pani", 
-            author_address:'Bengaluru', 
-            mobile:'7873160006', 
-            email:'mantupani17@gmail.com',
-            noOfAwards:10,
-            totalBooksPublished:100
+            "author_name":"mantu pani", 
+            "author_address":'Bengaluru', 
+            "mobile":'7873160006', 
+            "email":'mantupani17@gmail.com',
+            "author_dob":new Date("1991/11/5"),
+            "noOfAwards" : 4,
+            "totalBooksPublished" : 100,
+            "totalBooksSold" : 1000,
+            "awards" : [ 
+                {
+                    "award_name" : "ABC",
+                    "year" : 1988
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 1985
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 1989
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2000
+                }
+            ],
+            "sold_books" : [ 
+                {
+                    "book_id" : 1,
+                    "price" : 200,
+                    "totalSold" : 500
+                }, 
+                {
+                    "book_id" : 2,
+                    "price" : 500,
+                    "totalSold" : 500
+                }
+            ]
         },
         {
-            author_name:"mantu pani 1", 
-            author_address:'Bengaluru', 
-            mobile:'7873160003', 
-            email:'mantupani16@gmail.com',
-            noOfAwards:20,
-            totalBooksPublished:150
+            "author_name" : "mantu pani 1",
+            "author_address" : "Bengaluru",
+            "mobile" : "7873160003",
+            "email" : "mantupani16@gmail.com",
+            "noOfAwards" : 2,
+            "totalBooksPublished" : 150,
+            "totalBooksSold" : 1000,
+            "awards" : [ 
+                {
+                    "award_name" : "ABC",
+                    "year" : 1988
+                }, 
+                {
+                    "award_name" : "XYZ",
+                    "year" : 1985
+                }
+            ],
+            "sold_books" : [ 
+                {
+                    "book_id" : 1,
+                    "price" : 200,
+                    "totalSold" : 200
+                }, 
+                {
+                    "book_id" : 2,
+                    "price" : 500,
+                    "totalSold" : 500
+                }, 
+                {
+                    "book_id" : 3,
+                    "price" : 700,
+                    "totalSold" : 300
+                }
+            ],
+            "author_dob":new Date("1989/11/5")
         },
         {
-            author_name:"mantu pani 2", 
-            author_address:'Bengaluru', 
-            mobile:'7873160002', 
-            email:'mantupani15@gmail.com',
-            noOfAwards:2,
-            totalBooksPublished:1500
+            "author_name" : "mantu pani 3",
+            "author_address" : "Bengaluru",
+            "mobile" : "7873160004",
+            "email" : "mantupani14@gmail.com",
+            "noOfAwards" : 4,
+            "totalBooksPublished" : 1500,
+            "totalBooksSold" : 200,
+            "awards" : [ 
+                {
+                    "award_name" : "MNO",
+                    "year" : 1988
+                }, 
+                {
+                    "award_name" : "XYZ",
+                    "year" : 1985
+                }, 
+                {
+                    "award_name" : "ZYX",
+                    "year" : 1989
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2000
+                }
+            ],
+            "sold_books" : [ 
+                {
+                    "book_id" : 1,
+                    "price" : 200,
+                    "totalSold" : 300
+                }, 
+                {
+                    "book_id" : 2,
+                    "price" : 500,
+                    "totalSold" : 300
+                }, 
+                {
+                    "book_id" : 3,
+                    "price" : 700,
+                    "totalSold" : 500
+                }, 
+                {
+                    "book_id" : 4,
+                    "price" : 700,
+                    "totalSold" : 400
+                }
+            ],
+            "author_dob":new Date("1985/11/5")
         },
         {
-            author_name:"mantu pani 3", 
-            author_address:'Bengaluru', 
-            mobile:'7873160004', 
-            email:'mantupani14@gmail.com',
-            noOfAwards:4,
-            totalBooksPublished:1500
+            "author_name" : "mantu pani 2",
+            "author_address" : "Bengaluru",
+            "mobile" : "7873160002",
+            "email" : "mantupani15@gmail.com",
+            "noOfAwards" : 4,
+            "totalBooksPublished" : 1500,
+            "totalBooksSold" : 100,
+            "awards" : [ 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2002
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2001
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2003
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2000
+                }
+            ],
+            "sold_books" : [ 
+                {
+                    "book_id" : 1,
+                    "price" : 200,
+                    "totalSold" : 500
+                }, 
+                {
+                    "book_id" : 2,
+                    "price" : 500,
+                    "totalSold" : 500
+                }, 
+                {
+                    "book_id" : 3,
+                    "price" : 700,
+                    "totalSold" : 500
+                }
+            ],
+            author_dob:new Date("1990/11/5")
         },
         {
-            author_name:"mantu pani 4", 
-            author_address:'Bengaluru', 
-            mobile:'7873160005', 
-            email:'mantupani15@gmail.com',
-            noOfAwards:15,
-            totalBooksPublished:150
+            "author_name" : "mantu pani 4",
+            "author_address" : "Bengaluru",
+            "mobile" : "7873160005",
+            "email" : "mantupani15@gmail.com",
+            "noOfAwards" : 5,
+            "totalBooksPublished" : 150,
+            "totalBooksSold" : 2000,
+            "awards" : [ 
+                {
+                    "award_name" : "MNO",
+                    "year" : 1988
+                }, 
+                {
+                    "award_name" : "XYZ",
+                    "year" : 1985
+                }, 
+                {
+                    "award_name" : "ZYX",
+                    "year" : 1989
+                }, 
+                {
+                    "award_name" : "ABC",
+                    "year" : 2000
+                }, 
+                {
+                    "award_name" : "PQR",
+                    "year" : 2000
+                }
+            ],
+            "sold_books" : [ 
+                {
+                    "book_id" : 1,
+                    "price" : 200,
+                    "totalSold" : 300
+                }, 
+                {
+                    "book_id" : 2,
+                    "price" : 500,
+                    "totalSold" : 300
+                }, 
+                {
+                    "book_id" : 3,
+                    "price" : 700,
+                    "totalSold" : 500
+                }, 
+                {
+                    "book_id" : 4,
+                    "price" : 700,
+                    "totalSold" : 400
+                }, 
+                {
+                    "book_id" : 5,
+                    "price" : 700,
+                    "totalSold" : 400
+                }, 
+                {
+                    "book_id" : 6,
+                    "price" : 400,
+                    "totalSold" : 100
+                }
+            ],
+            "author_dob":new Date("1987/11/5")
         }
     ]
     AuthorController.createAuthors(db, authors, function(error, data){
-        console.log(data)
         const result = {status:true, data:data};
         res.json(result);
     });
@@ -71,9 +267,26 @@ router.get('/author/year/:year', function(req, res){
     }
 });
 
-router.get('/author/:n', function(req, res){
+router.get('/author/profit/:author_id', function(req, res){
+    const db = client.db('my-test-db');
+    let author_id = req.params.author_id;
+    let where = {};
+    if(author_id){
+        if (ObjectId.isValid(author_id)) {
+            author_id = ObjectId(author_id);
+            where = { _id:author_id };
+        }
+    }
+    AuthorController.getAllTotalProfitByAuthor(db,  where , {}, function(error, data){
+        const result = {status:true, data:data};
+        res.json(result);
+    })
+});
+
+router.get('/author/awards/:n', function(req, res){
     const db = client.db('my-test-db');
     var noOfAwards = req.params.n || 0;
+    console.log("condition=====")
     if(noOfAwards){
         noOfAwards = parseInt(noOfAwards);
         AuthorController.getAuthorsByNoOfAwards(db, {noOfAwards:{$gte:noOfAwards}} , function(error, data){
@@ -83,5 +296,25 @@ router.get('/author/:n', function(req, res){
         })
     }
 });
+
+router.get('/author/dob/:author_dob/totalPrice/:totalPrice', function(req, res){
+    const db = client.db('my-test-db');
+    let author_dob = req.params.author_dob;
+    let totalPrice = req.params.totalPrice;
+    let where = {};
+    let afterWhere = {};
+    if(author_dob){
+        where['author_dob'] = { $gte : new Date(author_dob) }
+    }
+    if(totalPrice){
+        afterWhere['totalProfit'] = {$gte:parseInt(totalPrice)}
+    }
+    console.log({where ,afterWhere})
+    AuthorController.getAllTotalProfitByAuthor(db,  where , afterWhere, function(error, data){
+        const result = {status:true, data:data};
+        res.json(result);
+    })
+});
+
 
 module.exports = router;
